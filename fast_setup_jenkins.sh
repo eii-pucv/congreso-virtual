babab43f185c13615059e232606673b301a9fc6c
elsemieni <elsemi.en.i@gmail.com>
2020-02-28 17:40:19 -0300

    Adding Jenkinsfile

--- fast_setup_jenkins.sh
+++ fast_setup_jenkins.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo -e $CONGRESO_ENV > .env
+echo -e $CONGRESO_ENV > .env_ex
+sed -e ':a' -e 'N' -e '$!ba' -e 's/\n /\n/g' .env_ex > .env
 ls -lah
+cat .env
 
 if [ ! -d "dist" ]; then
 	./fast_setup.sh
