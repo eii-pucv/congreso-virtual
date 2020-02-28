703b8e2c8bbd13be837c6669be67dfb948335287
elsemieni <elsemi.en.i@gmail.com>
2020-02-28 16:15:50 -0300

    Adding Jenkinsfile

--- fast_setup.sh
+++ fast_setup.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
 #Check docker
-if  which docker >/dev/null  &&  which docker-compose >/dev/null &&  test -f ./.env ; then
-	chmod 777 compile_frontend.sh configure.sh livelog.sh run.sh stop.sh fast_update.sh
-	./configure.sh --prepare=$UID
-	echo "CONGRESO_USER_UID=${UID}" >> ./.env
-	mv -f .env ./dist/volumefiles/
-	./configure.sh --applyconfig
-	./compile_frontend.sh
-	./run.sh
-else
-	echo -e "This script requires Docker, Docker compose and a .env file in this dir. See README for more details"
+if ! which docker >/dev/null ; then
+	echo "Docker not found. "
+	echo "This script requires Docker, Docker compose and a .env file in this dir. See README for more details."
+	exit 1
 fi
+	
+#Check docker compose
+if ! which docker-compose >/dev/null ; then
+	echo "Docker-compose not found. "
+	echo "This script requires Docker, Docker compose and a .env file in this dir. See README for more details."
+	exit 1
+fi	
+
+#Check env
+if ! test -f ./.env ; then
+	echo "env file not found. "
+	echo "This script requires Docker, Docker compose and a .env file in this dir. See README for more details."
+	exit 1
+fi
+
+chmod 777 compile_frontend.sh configure.sh livelog.sh run.sh stop.sh fast_update.sh installinitialdata.sh
+./configure.sh --prepare=$UID
+echo "CONGRESO_USER_UID=${UID}" >> ./.env
+mv -f .env ./dist/volumefiles/
+./configure.sh --applyconfig
+./compile_frontend.sh
+./run.sh
+
--- fast_setup_jenkins.sh
+++ fast_setup_jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ls -lah 
+
 if [ ! -d "dist" ]; then
 	./fast_setup.sh
 else 
