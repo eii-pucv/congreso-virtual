f727096fcb0db8ff823b36c939b673afde873f63
elsemieni <elsemi.en.i@gmail.com>
2020-03-16 14:25:42 -0300

    Refactoring and standarized error messages for Congreso Analytics

--- congresovirtual-data-analytics/Ngramas.py
+++ congresovirtual-data-analytics/Ngramas.py
@@ -3,8 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from plotly.offline import init_notebook_mode, plot, iplot
-#import plotly
-#import plotly.graph_objs as go
 
 import string
 import re
@@ -13,7 +11,7 @@ from nltk.corpus import stopwords
 import json
 import dbconfig
 
-def ngrams(proyect,words):
+def ngrams(project,words):
     
     #Conect MySQL
     mydb = mysql.connector.connect(
@@ -24,17 +22,15 @@ def ngrams(proyect,words):
         ssl_disabled=True
     )
     
-    textos = pd.read_sql('select * from comments where project_id='+ str(proyect), con=mydb)
-    #stop_words = pd.read_sql('select * from comments where project_id='+ str(proyect), con=mydb)
+    textos = pd.read_sql('select * from comments where project_id='+ str(project), con=mydb)
     textos = textos.body
     print(textos)
     
-  
     cv = CountVectorizer(stop_words='english', ngram_range=(words,words))
     try:
         text_processed = cv.fit_transform(textos)
     except ValueError:
-        return json.dumps({"error": "Vocabulario vacio. Aparentemente parametros faltantes o erroneos."})
+        return json.dumps({"error": "ANALYTICS_ERR_EMPTY_VOCABULARY", "errordescription": "Empty vocabulary. Unable to continue"})
 
     s = np.array(np.sum(text_processed,axis=0))
     sort = np.array(s).argsort()[0][::-1]
@@ -42,32 +38,13 @@ def ngrams(proyect,words):
     names = []
     for key,values in cv.vocabulary_.items():
         if values in sort[:40]:
-            #print(key,"\t Ocurrencia: ",s[0][values])
             names.append(key)
 
     values = s[0][sort[:40]]
-    #names = cv.inverse_transform(sort[:20])[0]
-    #names
 
     results = {}
     results["names"] = names
     results["values"] = values.tolist()
-    #trace0 = go.Bar(
-    #    x=names,
-    #    y=values,
-    #    name='Frecuencia de N-gramas'
-    #)
 
-    #data = [trace0]
-    #layout = {
-    #    'title': 'Frecuencia de '+str(words)+'-gramas',
-    #    'margin':go.layout.Margin(l=50,r=50,b=100,t=100,pad=4)
-    #}
     print(json.dumps(results))
-
-    #fig = go.Figure(data=data, layout=layout)
-    #iplot(fig, show_link=False,image_height=800)
-    #fig.write_image("img/"+str(words)+"-grama.jpeg")
     return json.dumps(results)
-        
-    #return iplot(fig, show_link=False,image_height=800)
